7d5b4b17d309f24b1941c8ffaa6ed7a6e9125ee6
Artiom G. <artiomgarbul@gmail.com>
2025-03-21 19:25:57 +0200

    Made design for game states in aim trainer

--- frontend/src/pages/games/AimTrainer.tsx
+++ frontend/src/pages/games/AimTrainer.tsx
@@ -1,18 +1,80 @@
+import React,{useState,useEffect,useRef} from "react"
 import "./AimTrainer.css"
 
-export const AimTrainer=() => {
+interface Target{
+    x: number;
+    y: number;
+}
+
+const AimTrainer: React.FC=() => {
+    const [gameState,setGameState]=useState("main");
+
+    const startGame=async() => {
+        setGameState("started");
+    }
+
     return(
-        <div className="game_window row">
-            <div className="personal-stats">
-                Highscore
-                Times played
-            </div>
-            <div className="start_button">
-                Start
-            </div>
-            <div className="leaderboard">
-                Leaderboard
-            </div>
+        <div className="game_window">
+            {gameState=="main" ? (
+                <>
+                    <div className="game_title">
+                        Melon Ninja
+                    </div>
+                    <div className="main row">
+                        <div className="personal_stats">
+                            <div className="highscore">
+                                Highscore
+                            </div>
+                            <hr/>
+                            <div className="highscore_num">
+                                154
+                            </div>
+                            <div className="times_played">
+                                Times played
+                            </div>
+                            <hr/>
+                            <div className="times_played_num">
+                                40
+                            </div>
+                        </div>
+                        <button onClick={startGame} className="start_button">Start</button>
+                        <div className="leaderboard">
+                            Leaderboard
+                            <hr/>
+                            <div className="leaderboard_values">
+                                1. Artiom
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ):(
+                <>
+                    {gameState=="started" ? (
+                        <>
+                            <div className="col">
+                                <div className="row">
+                                    <div className="game_score">
+                                        Score:
+                                    </div>
+                                    <div className="game_timer">
+                                        Time left:
+                                    </div>
+                                </div>
+                                <div className="game_area">
+
+                                </div>
+                            </div>
+                        </>
+                    ):(
+                        <>
+
+                        </>
+                    )}
+                </>
+            )}
+            
         </div>
     )
 }
+
+export default AimTrainer
