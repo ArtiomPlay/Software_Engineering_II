b1261b006d6e673adeeef964c55b30ca9ba7ff48
Artiom G. <artiomgarbul@gmail.com>
2025-04-03 21:28:36 +0300

    Made leaderboard page concept

--- frontend/src/pages/main/Leaderboard.tsx
+++ frontend/src/pages/main/Leaderboard.tsx
@@ -2,8 +2,117 @@ import "./Leaderboard.css"
 
 export const Leaderboard=() => {
     return(
-        <div>
-            Leaderboard
+        <div className="col">
+            <div className="row">
+                <div className="all_score_table">
+                    <div className="table_type">
+                        From all games
+                    </div>
+                    <hr/>
+                    <div className="table_title">
+                        Score:
+                    </div>
+                    <div className="table_value">
+                        409132
+                    </div>
+                    <div className="table_title">
+                        Times played:
+                    </div>
+                    <div className="table_value">
+                        1240
+                    </div>
+                </div>
+                <div className="all_score_table">
+                    <div className="table_type">
+                        Aim Trainer
+                    </div>
+                    <hr/>
+                    <div className="table_title">
+                        Score:
+                    </div>
+                    <div className="table_value">
+                        124534
+                    </div>
+                    <div className="table_title">
+                        Times played:
+                    </div>
+                    <div className="table_value">
+                        114
+                    </div>
+                </div>
+                <div className="all_score_table">
+                    <div className="table_type">
+                        Math Game
+                    </div>
+                    <hr/>
+                    <div className="table_title">
+                        Score:
+                    </div>
+                    <div className="table_value">
+                        124534
+                    </div>
+                    <div className="table_title">
+                        Times played:
+                    </div>
+                    <div className="table_value">
+                        114
+                    </div>
+                </div>
+                <div className="all_score_table">
+                    <div className="table_type">
+                        Seeker
+                    </div>
+                    <hr/>
+                    <div className="table_title">
+                        Score:
+                    </div>
+                    <div className="table_value">
+                        124534
+                    </div>
+                    <div className="table_title">
+                        Times played:
+                    </div>
+                    <div className="table_value">
+                        114
+                    </div>
+                </div>
+                <div className="all_score_table">
+                    <div className="table_type">
+                        Sequence
+                    </div>
+                    <hr/>
+                    <div className="table_title">
+                        Score:
+                    </div>
+                    <div className="table_value">
+                        124534
+                    </div>
+                    <div className="table_title">
+                        Times played:
+                    </div>
+                    <div className="table_value">
+                        114
+                    </div>
+                </div>
+                <div className="all_score_table">
+                    <div className="table_type">
+                        Typing
+                    </div>
+                    <hr/>
+                    <div className="table_title">
+                        Score:
+                    </div>
+                    <div className="table_value">
+                        124534
+                    </div>
+                    <div className="table_title">
+                        Times played:
+                    </div>
+                    <div className="table_value">
+                        114
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
